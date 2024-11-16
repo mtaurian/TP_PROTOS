bdfe1e7918a9686d5768061488bc94f97463981b
Diego Badin <dbadin@itba.edu.ar>
2024-11-16 00:12:29 -0300

    Improved error handling in main.c

--- src/server/main.c
+++ src/server/main.c
@@ -72,8 +72,8 @@ int main(const int argc, const char **argv) {
 
     int server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (server_fd < 0) {
-        perror("unable to create socket");
-        exit(EXIT_FAILURE); // goto finally
+        err_msg = "unable to create socket";
+        goto finally;
     }
 
     fprintf(stdout, "Listening on TCP port %d\n", port);
@@ -83,16 +83,14 @@ int main(const int argc, const char **argv) {
 
     // Asociar el socket a la dirección y puerto
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Error en bind");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+        err_msg = "unable to bind socket";
+        goto finally;
     }
 
     // Escuchar conexiones entrantes
     if (listen(server_fd, 20) < 0) {
-        perror("Error en listen");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+        err_msg = "unable to listen";
+        goto finally;
     }
 
 
@@ -115,13 +113,13 @@ int main(const int argc, const char **argv) {
     };
 
     if(0 != selector_init(&conf)) {
-        perror("unable to initialize selector");
+        err_msg = "unable to initialize selector";
         goto finally;
     }
 
     selector = selector_new(1024);
     if(selector == NULL) {
-        perror("unable to create selector");
+        err_msg = "unable to create selector";
         goto finally;
     }
 
@@ -132,9 +130,9 @@ int main(const int argc, const char **argv) {
     pop3->handle_close = NULL;
 
     // register as reader
-    ss = selector_register(selector, server_fd, pop3,OP_READ, NULL);
+    ss = selector_register(selector, server_fd, &pop3,OP_READ, NULL);
     if(ss != SELECTOR_SUCCESS) {
-        printf("unable to register socket handler\n");
+        err_msg = "registering fd";
         goto finally;
     }
 
@@ -142,147 +140,6 @@ int main(const int argc, const char **argv) {
 
 
     for(;!done;) {
-        ss = selector_select(selector);
-        if(ss != SELECTOR_SUCCESS) {
-            perror("Error en selector_select");
-            goto finally;
-        }
-    }
-
-    int ret = 0;
-finally:
-    /*if(ss != SELECTOR_SUCCESS) {
-        fprintf(stderr, "%s: %s\n", (err_msg == NULL) ? "": err_msg,
-                                  ss == SELECTOR_IO
-                                      ? strerror(errno)
-                                      : selector_error(ss));
-        ret = 2;
-    } else if(err_msg) {
-        perror(err_msg);
-        ret = 1;
-    }*/
-    if(selector != NULL) {
-        selector_destroy(selector);
-    }
-    selector_close();
-
-    // socksv5_pool_destroy();
-
-    if(server_fd >= 0) {
-        close(server_fd);
-        printf("Servidor cerrado.\n");
-    }
-
-    free(pop3);
-    return ret;
-
-}
-
-
-
-
-/*
-static void
-sigterm_handler(const int signal) {
-    printf("signal %d, cleaning up and exiting\n",signal);
-    done = true;
-}
-
-int
-main(const int argc, const char **argv) {
-    unsigned port = 1080;
-
-    if(argc == 1) {
-        // utilizamos el default
-    } else if(argc == 2) {
-        char *end     = 0;
-        const long sl = strtol(argv[1], &end, 10);
-
-        if (end == argv[1]|| '\0' != *end
-           || ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno)
-           || sl < 0 || sl > USHRT_MAX) {
-            fprintf(stderr, "port should be an integer: %s\n", argv[1]);
-            return 1;
-        }
-        port = sl;
-    } else {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-        return 1;
-    }
-
-    // no tenemos nada que leer de stdin
-    close(0);
-
-    const char       *err_msg = NULL;
-    selector_status   ss      = SELECTOR_SUCCESS;
-    fd_selector selector      = NULL;
-
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family      = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port        = htons(port);
-
-    const int server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(server < 0) {
-        err_msg = "unable to create socket";
-        goto finally;
-    }
-
-    fprintf(stdout, "Listening on TCP port %d\n", port);
-
-    // man 7 ip. no importa reportar nada si falla.
-    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
-
-    if(bind(server, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
-        err_msg = "unable to bind socket";
-        goto finally;
-    }
-
-    if (listen(server, 20) < 0) {
-        err_msg = "unable to listen";
-        goto finally;
-    }
-
-    // registrar sigterm es útil para terminar el programa normalmente.
-    // esto ayuda mucho en herramientas como valgrind.
-    signal(SIGTERM, sigterm_handler);
-    signal(SIGINT,  sigterm_handler);
-
-    if(selector_fd_set_nio(server) == -1) {
-        err_msg = "getting server socket flags";
-        goto finally;
-    }
-    const struct selector_init conf = {
-        .signal = SIGALRM,
-        .select_timeout = {
-            .tv_sec  = 10,
-            .tv_nsec = 0,
-        },
-    };
-    if(0 != selector_init(&conf)) {
-        err_msg = "initializing selector";
-        goto finally;
-    }
-
-    selector = selector_new(1024);
-    if(selector == NULL) {
-        err_msg = "unable to create selector";
-        goto finally;
-    }
-    const struct fd_handler socksv5 = {
-    .handle_read       = socksv5_passive_accept,
-    .handle_write      = NULL,
-    .handle_close      = NULL, // nada que liberar
-    };
-    ss = selector_register(selector, server, &socksv5,OP_READ, NULL);
-    if(ss != SELECTOR_SUCCESS) {
-        err_msg = "registering fd";
-        goto finally;
-    }
-
-    for(;!done;) {
-        err_msg = NULL;
         ss = selector_select(selector);
         if(ss != SELECTOR_SUCCESS) {
             err_msg = "serving";
@@ -312,9 +169,11 @@ finally:
 
     // socksv5_pool_destroy();
 
-    if(server >= 0) {
-        close(server);
+    if(server_fd >= 0) {
+        close(server_fd);
+        printf("Servidor cerrado.\n");
     }
+
     return ret;
+
 }
-*/
