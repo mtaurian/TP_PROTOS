dec09d11cc70797cd9c259ee43a1f33bc15f5988
Diego-Rabinovich <120027298+Diego-Rabinovich@users.noreply.github.com>
2024-11-18 00:36:36 -0300

    Implemented auth_user state

---
+++ src/server/states_definition/auth_user.c
@@ -0,0 +1,88 @@
+
+#include "include/auth_user.h"
+#include "../include/handlers.h"
+#include "include/utils.h"
+#include "../../shared/include/parser_utils.h"
+#include "../include/pop3.h"
+
+static auth_user_request * parse(struct selector_key * key){
+    client_data * clientData= ATTACHMENT(key);
+
+    fd_to_client_buffer(clientData, key);
+
+    uint8_t entry = buffer_read(&clientData->clientBuffer);
+
+    //TODO: Change all this command compares to parse_utils :)
+    char match = 1;
+    char maybeUser = 1;
+    char maybeQuit = 1;
+    const char * userCmd = "USER";
+    const char * quitCmd = "QUIT";
+    while(match && entry != ' ' && entry) {
+        if (maybeUser && (entry == *userCmd || entry == *userCmd + 32)) {
+            userCmd++;
+            maybeQuit = 0;
+        } else if (maybeQuit && (entry == *quitCmd || entry == *quitCmd + 32)) {
+            quitCmd++;
+            maybeUser = 0;
+        } else {
+            match = 0;
+        }
+            entry = buffer_read(&clientData->clientBuffer);
+    }
+
+    auth_user_request *request = malloc(sizeof(auth_user_request));
+
+    if(match){
+        request->command = maybeQuit ? QUIT : USER;
+    }
+
+    size_t toRead;
+    buffer_read_ptr(&clientData->clientBuffer, &toRead);
+
+    entry = buffer_read(&clientData->clientBuffer);
+    request->payload = malloc((toRead+1) * sizeof(uint8_t));
+    int i;
+    for(i = 0; entry && i < toRead; i++){
+        request->payload[i] = (char) entry;
+        entry = buffer_read(&clientData->clientBuffer);
+    }
+    request->payload[i] = '\0';
+    return request;
+}
+
+void auth_user_on_arrival(const unsigned state, struct selector_key *key){
+    printf("Entered in AUTH_USER state\n");
+}
+
+void auth_user_on_departure(const unsigned state, struct selector_key *key){
+    printf("Exited AUTH_USER state\n");
+}
+
+unsigned int auth_user_on_ready_to_read(struct selector_key *key){
+    auth_user_request * entry = parse(key);
+    int ret = AUTHORIZATION_USER;
+
+    switch (entry->command) {
+        case USER:
+            if(handle_user(key, entry->payload)){
+                ret = AUTHORIZATION_PASSWORD;
+            }
+            break;
+        case QUIT:
+            handle_quit(key);
+            break;
+        default:
+            write_std_response(0, NULL, key);
+            break;
+    }
+
+    return ret; //TODO: leak
+}
+
+unsigned int auth_user_on_ready_to_write(struct selector_key *key){
+    print_response(key);
+    return ATTACHMENT(key)->stm.current->state;
+}
+
+
---
+++ src/server/states_definition/include/auth_user.h
@@ -0,0 +1,20 @@
+#ifndef TP_PROTOS_AUTH_USER_H
+#define TP_PROTOS_AUTH_USER_H
+
+#include "../../../shared/include/selector.h"
+
+enum auth_user_commands {
+    USER = 0, QUIT
+};
+
+typedef struct auth_user_request {
+    enum auth_user_commands command;
+    char * payload;
+} auth_user_request;
+
+void auth_user_on_arrival(unsigned state, struct selector_key *key);
+void auth_user_on_departure(unsigned state, struct selector_key *key);
+unsigned int auth_user_on_ready_to_read(struct selector_key *key);
+unsigned int auth_user_on_ready_to_write(struct selector_key *key);
+
+#endif //TP_PROTOS_AUTH_USER_H
