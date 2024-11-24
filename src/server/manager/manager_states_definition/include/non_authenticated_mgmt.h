4d6fbecbdfe62ec640323b754033a87f069d6699
Diego-Rabinovich <120027298+Diego-Rabinovich@users.noreply.github.com>
2024-11-24 19:12:56 -0300

    first step on non_authenticated mgmt state

---
+++ src/server/manager/manager_states_definition/include/non_authenticated_mgmt.h
@@ -0,0 +1,7 @@
+#ifndef TP_PROTOS_NON_AUTHENTICATED_MGMT_H
+#define TP_PROTOS_NON_AUTHENTICATED_MGMT_H
+#include <stdio.h>
+#include "../../../../shared/include/selector.h"
+void non_authenticated_on_arrival(unsigned state, struct selector_key *key);
+
+#endif //TP_PROTOS_NON_AUTHENTICATED_MGMT_H
